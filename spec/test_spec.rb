ef95558a35705cc3270f95564d63848362f88cfc
helio <matsubayashi.helio@gmail.com>
2021-05-12 05:04:17 -0300

    WIP refatoração de testes

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,11 +1,9 @@
-'test rspec file'
 require 'spec_helper'
 require 'cli'
 require 'name'
 require 'byebug'
 require 'json'
 require 'stringio'
-require_relative '../opt'
 
 
 describe Cli do
@@ -14,13 +12,14 @@ describe Cli do
         it 'should show welcome message' do
             io = StringIO.new
             io.puts 'asdfasdfasdf'
-            io.puts 'aaaaaaaaaa'
+            # io.puts 'aaaaaaaaaa'
             io.rewind
             $stdin = io
             # byebug 
-
+            city = Cli.get_city_name
             # expect {Cli.welcome}.to output("asdfasdfasdf").to_stdout
-            expect {Cli.welcome}.to output(include("ASDFASDFASDF","aaaaaaaaaa")).to_stdout
+            # expect {Cli.get_city_name}.to output(include("ASDFASDFASDF","aaaaaaaaaa")).to_stdout
+            expect(city).to eq 'asdfasdfasdf'
 
             $stdin = STDIN
         end
